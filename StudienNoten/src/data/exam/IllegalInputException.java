b06201c59fb04a0ec7248222052577fab0cf1b56
bejoeb11 <bejo.eb11@gmail.com>
2019-09-06 13:50:27 +0200

    changes were made

---
+++ StudienNoten/src/data/exam/IllegalInputException.java
@@ -0,0 +1,12 @@
+package data.exam;
+
+public class IllegalInputException extends Exception {
+    /**
+     *
+     */
+    private static final long serialVersionUID = 6502884778716528608L;
+
+    public IllegalInputException(String message) {
+        super(message);
+    }
+}
