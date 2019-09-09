555e113b676f9c2ad9e93f3a9c37c48261957065
seb711 <sebastian@kosaks.de>
2019-09-09 14:17:35 +0200

    table show not passed exams and marks them red

--- StudienNoten/src/gui/mainFrame/TableExams.java
+++ StudienNoten/src/gui/mainFrame/TableExams.java
@@ -1,6 +1,9 @@
 package gui.mainFrame;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.Dimension;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Vector;
@@ -11,61 +14,77 @@ import javax.swing.JTable;
 import javax.swing.RowSorter;
 import javax.swing.SortOrder;
 import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableCellRenderer;
 import javax.swing.table.TableModel;
 import javax.swing.table.TableRowSorter;
 
 import data.exam.Exam;
 import data.exam.ExamContainer;
 
-public class TableExams extends JPanel{
+public class TableExams extends JPanel {
 
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1536495359476264660L;
 
-	public Model model;
 	public JTable tblTaskList;
 	private DefaultTableModel dm;
-	private ExamContainer container; 
+	private ExamContainer container;
 
 	@SuppressWarnings("serial")
 	public TableExams(ExamContainer container) {
 
 		super();
-		this.container = container; 
-		tblTaskList = new JTable();
-		tblTaskList.setShowVerticalLines(true);
-		tblTaskList.setCellSelectionEnabled(false);
-		tblTaskList.setRowSelectionAllowed(true);
-
-		((JPanel) this).add(tblTaskList);
+		this.container = container;
 
 		// create object of table and table model
-		dm = new DefaultTableModel(0, 0){
+		dm = new DefaultTableModel(0, 0) {
 			@Override
 			public boolean isCellEditable(int row, int column) {
-				//all cells false
+				// all cells false
 				return false;
 			}
 		};
 		String header[] = new String[] { "Semester", "LPs", "Name", "Note" };
 		dm.setColumnIdentifiers(header);
-		tblTaskList.setModel(dm);
-		TableRowSorter<TableModel> sorter = new TableRowSorter<TableModel>(tblTaskList.getModel()); 
+		tblTaskList = new JTable(dm) {
+			@Override
+			public Dimension getPreferredScrollableViewportSize() {
+				return new Dimension(350, 150);
+			}
+
+			@Override
+			public Component prepareRenderer(TableCellRenderer renderer, int row, int col) {
+				Component c = super.prepareRenderer(renderer, row, col);
+				String status = (String) getValueAt(row, 3);
+				if (Double.parseDouble(status) > 4.0) {
+					c.setBackground(Color.RED);
+					c.setForeground(Color.WHITE);
+				} else {
+					c.setBackground(super.getBackground());
+					c.setForeground(super.getForeground());
+				}
+				return c;
+			}
+		};
+		tblTaskList.setShowVerticalLines(true);
+		tblTaskList.setCellSelectionEnabled(false);
+		tblTaskList.setRowSelectionAllowed(true);
+		TableRowSorter<TableModel> sorter = new TableRowSorter<TableModel>(tblTaskList.getModel());
 		tblTaskList.setRowSorter(sorter);
 		sorter.setSortsOnUpdates(true);
-		
-		List<RowSorter.SortKey> sortKeys = new ArrayList<>(25); 
-		sortKeys.add(new RowSorter.SortKey(0, SortOrder.ASCENDING)); 
+
+		List<RowSorter.SortKey> sortKeys = new ArrayList<>(25);
+		sortKeys.add(new RowSorter.SortKey(0, SortOrder.ASCENDING));
 		sortKeys.add(new RowSorter.SortKey(1, SortOrder.DESCENDING));
-		sortKeys.add(new RowSorter.SortKey(2, SortOrder.ASCENDING)); 
+		sortKeys.add(new RowSorter.SortKey(2, SortOrder.ASCENDING));
 		sorter.setSortKeys(sortKeys);
 
-		load(); 
+		load();
 
 		tblTaskList.getColumn("Semester").setPreferredWidth(20);
-		tblTaskList.getColumn("Name").setPreferredWidth(200); 
+		tblTaskList.getColumn("Name").setPreferredWidth(200);
 		tblTaskList.getColumn("LPs").setPreferredWidth(10);
 		tblTaskList.getColumn("Note").setPreferredWidth(10);
 		this.setLayout(new BorderLayout());
@@ -73,32 +92,54 @@ public class TableExams extends JPanel{
 
 	}
 
-
-		/*
-		 * Vector<String> columnNames = new Vector<String>(); Vector<Vector<String>>
-		 * rowData = new Vector<Vector<String>>(); columnNames.addElement("Semester");
-		 * columnNames.addElement("Name"); columnNames.addElement("Leistungspunkte");
-		 * columnNames.addElement("Note");
-		 * 
-		 * for (int i = 0; i < container.getSize(); i++) { System.out.println("Hello");
-		 * Exam e = container.getExamByIndex(i); Vector<String> temp = new
-		 * Vector<String>(); temp.add(Integer.toString(e.getSemester()));
-		 * temp.add(Integer.toString(e.getLeistungpunkte())); temp.add(e.getName());
-		 * temp.add(Double.toString(e.getNote())); rowData.add(temp);
-		 * System.out.println("geooo"); }
-		 * 
-		 * dataTable = new JTable(rowData, columnNames);
-		 * 
-		 * dataTable.getColumn("Semester").setPreferredWidth(20);
-		 * dataTable.getColumn("Name").setPreferredWidth(200);
-		 * dataTable.getColumn("Leistungspunkte").setPreferredWidth(10);
-		 * dataTable.getColumn("Note").setPreferredWidth(10); this.setLayout(new
-		 * BorderLayout()); this.add(new JScrollPane(dataTable));
-		 */
+	/*
+	 * Vector<String> columnNames = new Vector<String>(); Vector<Vector<String>>
+	 * rowData = new Vector<Vector<String>>(); columnNames.addElement("Semester");
+	 * columnNames.addElement("Name"); columnNames.addElement("Leistungspunkte");
+	 * columnNames.addElement("Note");
+	 * 
+	 * for (int i = 0; i < container.getSize(); i++) { System.out.println("Hello");
+	 * Exam e = container.getExamByIndex(i); Vector<String> temp = new
+	 * Vector<String>(); temp.add(Integer.toString(e.getSemester()));
+	 * temp.add(Integer.toString(e.getLeistungpunkte())); temp.add(e.getName());
+	 * temp.add(Double.toString(e.getNote())); rowData.add(temp);
+	 * System.out.println("geooo"); }
+	 * 
+	 * dataTable = new JTable(rowData, columnNames);
+	 * 
+	 * dataTable.getColumn("Semester").setPreferredWidth(20);
+	 * dataTable.getColumn("Name").setPreferredWidth(200);
+	 * dataTable.getColumn("Leistungspunkte").setPreferredWidth(10);
+	 * dataTable.getColumn("Note").setPreferredWidth(10); this.setLayout(new
+	 * BorderLayout()); this.add(new JScrollPane(dataTable));
+	 */
 	public void load() {
-		while(dm.getRowCount() > 0)
-		{
-		    dm.removeRow(0);
+		while (dm.getRowCount() > 0) {
+			dm.removeRow(0);
+		}
+		for (int count = 1; count <= container.getSize(); count++) {
+			Exam e = container.getExamByIndex(count - 1);
+			if (e.getNote() > 4.0) {
+				continue;
+			} else {
+				Vector<String> data = new Vector<String>();
+				data.add(Integer.toString(e.getSemester()));
+				data.add(Integer.toString(e.getLeistungpunkte()));
+				data.add(e.getName());
+				data.add(Double.toString(e.getNote()));
+				dm.addRow(data);
+			}
+		}
+		revalidate();
+	}
+
+	public DefaultTableModel getDefaultTableModel() {
+		return dm;
+	}
+
+	public void loadAll() {
+		while (dm.getRowCount() > 0) {
+			dm.removeRow(0);
 		}
 		for (int count = 1; count <= container.getSize(); count++) {
 			Vector<String> data = new Vector<String>();
@@ -109,15 +150,21 @@ public class TableExams extends JPanel{
 			data.add(Double.toString(e.getNote()));
 			dm.addRow(data);
 		}
-		 revalidate();
+		for (int i = 0; i < dm.getRowCount(); i++) {
+			String status = (String) dm.getValueAt(i, 3);
+			if (Double.parseDouble(status) > 4.0) {
+				setBackground(Color.RED);
+				setForeground(Color.WHITE);
+			} else {
+				setBackground(tblTaskList.getBackground());
+				setForeground(tblTaskList.getForeground());
+			}
+		}
+		revalidate();
 	}
 
-	public DefaultTableModel getDefaultTableModel() {
-		return dm;
-	}
-	 
 	public JTable getTable() {
-		return tblTaskList; 
+		return tblTaskList;
 	}
 
 }
